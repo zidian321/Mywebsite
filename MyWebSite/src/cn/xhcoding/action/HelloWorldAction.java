5663b55ba1a8feafd6c21681ff128e1983905fb7
yhyyang <yhyyang@cn.ibm.com>
2018-02-12 18:01:30 +0800

    commit test

--- MyWebSite/src/cn/xhcoding/action/HelloWorldAction.java
+++ MyWebSite/src/cn/xhcoding/action/HelloWorldAction.java
@@ -4,7 +4,7 @@ public class HelloWorldAction extends com.opensymphony.xwork2.ActionSupport{
 
     @Override
     public String execute() throws Exception {
-        System.out.println("正在执行的Action");
+        System.out.println("正在执行的Actionfdfdsf");
         // 返回视图 SUCCESS，这是框架定义
         return SUCCESS;
     }
